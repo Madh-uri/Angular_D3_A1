c3aee076c7dfc4c1ebf9a5cc92d8390a5cef6f81
Madh-uri <128700998+Madh-uri@users.noreply.github.com>
2023-05-09 18:08:20 +0530

    Add files via upload

---
+++ Assignment_D3_1/datatypes.js
@@ -0,0 +1,12 @@
+//boolean
+var isCompleted = true;
+console.log("isCompleted:", isCompleted); // output: isCompleted: true
+//any
+var data = "Hello! Angular";
+console.log(data);
+//array of strings
+var fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits);
+//heterogenous array
+var userDetails = ["John", 21, "Manager"];
+console.log(userDetails);
---
+++ Assignment_D3_1/datatypes.ts
@@ -0,0 +1,16 @@
+//boolean
+var isCompleted: boolean = true;
+console.log("isCompleted:", isCompleted); // output: isCompleted: true
+
+//any
+var data : any = "Hello! Angular"
+console.log(data)
+
+//array of strings
+var fruits : string[] = ["Apple", "Banana", "Cherry"]
+console.log(fruits)
+
+//heterogenous array
+var userDetails : any[] = ["John",21,"Manager"]
+console.log(userDetails)
+
